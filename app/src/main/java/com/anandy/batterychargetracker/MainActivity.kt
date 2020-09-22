bf0d4954ecbfcc0280de8342192ce1ebde29473d
Andy Armaignac <andy.armaignac@doceleguas.com>
2020-09-21 22:42:45 -0400

    Created Main Activity layout with sample data

--- app/src/main/java/com/anandy/batterychargetracker/MainActivity.kt
+++ app/src/main/java/com/anandy/batterychargetracker/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.anandy.batterychargetracker.databinding.ActivityMainBinding
+import com.anandy.batterychargetracker.databinding.ContentMainBinding
+import com.anandy.batterychargetracker.model.BatteryChargeAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val recordsAdapter = BatteryChargeAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val mainContent: ContentMainBinding = binding.content
+        mainContent.batteryChargeRecycler.adapter = recordsAdapter
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/com/anandy/batterychargetracker/model/BatteryChargeAdapter.kt
@@ -0,0 +1,47 @@
+package com.anandy.batterychargetracker.model
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.anandy.batterychargetracker.R
+import com.anandy.batterychargetracker.databinding.BatteryChargeRecordBinding
+import com.anandy.batterychargetracker.inflate
+import com.anandy.batterychargetracker.populateItems
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+
+class BatteryChargeAdapter(private var items: List<BatteryCharge> = populateItems()): RecyclerView.Adapter<BatteryChargeAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.battery_charge_record)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        val nextItem = if(items.size > position + 1) items[position+1] else null
+        holder.bind(item, nextItem)
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val binding = BatteryChargeRecordBinding.bind(view)
+        private val format = SimpleDateFormat("EEEE, dd MMMM yyyy")
+
+        fun bind(item: BatteryCharge, nextItem: BatteryCharge?) {
+            binding.textChargeKM.text = item.kilometers.toString()
+            binding.textChargeDate.text = format.format(item.date)
+            binding.textKMDifference.text = ""
+            binding.textDateDifference.text = ""
+            nextItem?.apply {
+                val diffInMillisec: Long = item.date.time - date.time
+                val days = TimeUnit.MILLISECONDS.toDays(diffInMillisec)
+                binding.textKMDifference.text = "+${item.kilometers - kilometers} km"
+                binding.textDateDifference.text = "+ $days día(s)"
+            }
+        }
+    }
+}
